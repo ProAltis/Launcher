6032c65679af952b081efe3a1b4b2de9c0606853
SodiumFine <ibapple4@gmail.com>
2017-03-05 16:53:40 -0500

    Multi-toon support

--- ProjectAltisLauncher/Core/Play.cs
+++ ProjectAltisLauncher/Core/Play.cs
@@ -4,21 +4,25 @@ using System.Windows.Forms;
 
 namespace ProjectAltisLauncher.Core
 {
-    class Play
+    public static class Play
     {
+        /// <summary>
+        /// Launches the game.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
         public static void LaunchGame(string username, string password)
         {
             Environment.SetEnvironmentVariable("TT_USERNAME", username);
             Environment.SetEnvironmentVariable("TT_PASSWORD", password);
             Environment.SetEnvironmentVariable("TT_GAMESERVER", "gs1.projectaltis.com");
             ProcessStartInfo startInfo = new ProcessStartInfo();
-            if (Properties.Settings.Default.wantsGameDebug == false)
+            if (!Properties.Settings.Default.wantsGameDebug)
             {
                 startInfo.WindowStyle = ProcessWindowStyle.Hidden;
             }         
             startInfo.FileName = "ProjectAltis";
             Process.Start(startInfo);
-            Application.Exit();
         }
 
     }
