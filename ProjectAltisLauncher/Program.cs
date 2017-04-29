dbb0ce7803c686dc73c8cc9d4eaef880ea814e0d
judge2020 <hunterray55@hotmail.com>
2017-04-29 19:50:47 -0400

    Fix this branch

--- ProjectAltisLauncher/Program.cs
+++ ProjectAltisLauncher/Program.cs
@@ -10,44 +10,46 @@ using Squirrel;
 
 namespace ProjectAltisLauncher
 {
-    public static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        public static void Main()
-        {
-	        try
-	        {
-		        var AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-		        Properties.Settings.Default.filesDir = Path.Combine(AppDataPath, "Project Altis");
-		        if (!Directory.Exists(Properties.Settings.Default.filesDir))
-			        Directory.CreateDirectory(Properties.Settings.Default.filesDir);
+	public static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			try
+			{
+				var AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+				Properties.Settings.Default.filesDir = Path.Combine(AppDataPath, "Project Altis");
+				if (!Directory.Exists(Properties.Settings.Default.filesDir))
+					Directory.CreateDirectory(Properties.Settings.Default.filesDir);
 				Directory.SetCurrentDirectory(Properties.Settings.Default.filesDir);
-		        Log.Initialize(LogType.Info);
-		        Log.Info("Loaded altis launcher v" + typeof(Program).Assembly.GetName().Version);
-		        Log.Info("Current time: " + DateTime.Now.ToLongDateString() + "|" + DateTime.Now.TimeOfDay);
+				Log.Initialize(LogType.Info);
+				Log.Info("Loaded altis launcher v" + typeof(Program).Assembly.GetName().Version);
+				Log.Info("Current time: " + DateTime.Now.ToLongDateString() + "|" + DateTime.Now.TimeOfDay);
 				Log.Info("Working directory: " + Directory.GetCurrentDirectory());
-		        Application.EnableVisualStyles();
-		        Application.SetCompatibleTextRenderingDefault(false);
-		        Application.Run(new Forms.frmMain());
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(new Forms.frmMain());
 				//It'll update in the background while it's running. The update is in effect the next time they restart app.
-		        MainAsync().Wait();
+				MainAsync().Wait();
 			}
-	        catch (Exception e)
-	        {
-		        Log.Error(e);
-		        throw;
-	        }
+			catch (Exception e)
+			{
+				Log.Error(e);
+				throw;
+			}
 		}
 
-	    private static async Task MainAsync()
-	    {
-		    using (var manager = new UpdateManager("https://judge2020.com/altis"))
-		    {
-			    Log.Info("checking for update: " + manager.RootAppDirectory);
-			    await manager.UpdateApp();
-		    }
-    }
+		private static async Task MainAsync()
+		{
+			using (var manager = new UpdateManager("https://judge2020.com/altis"))
+			{
+				Log.Info("checking for update: " + manager.RootAppDirectory);
+				await manager.UpdateApp();
+			}
+
+		}
+	}
 }
