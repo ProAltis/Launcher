393de0891a713e082548b68c3c439c09c5c70f1c
judge2020 <hunterray55@hotmail.com>
2017-04-30 16:17:59 -0400

    Fix indentation in Program.cs

--- ProjectAltisLauncher/Program.cs
+++ ProjectAltisLauncher/Program.cs
@@ -1,54 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Reflection;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using ProjectAltisLauncher.Enums;
-using Splat;
+using ProjectAltisLauncher.Forms;
+using ProjectAltisLauncher.Properties;
 using Squirrel;
 
 namespace ProjectAltisLauncher
 {
-	public static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		public static void Main()
-		{
-			try
-			{
-				var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-				Properties.Settings.Default.filesDir = Path.Combine(appDataPath, "Project Altis");
-				if (!Directory.Exists(Properties.Settings.Default.filesDir))
-					Directory.CreateDirectory(Properties.Settings.Default.filesDir);
-				Directory.SetCurrentDirectory(Properties.Settings.Default.filesDir);
-				Log.Initialize(LogType.Info);
-				Log.Info("Loaded altis launcher v" + typeof(Program).Assembly.GetName().Version);
-				Log.Info("Current time: " + DateTime.Now.ToLongDateString() + "|" + DateTime.Now.TimeOfDay);
-				Log.Info("Working directory: " + Directory.GetCurrentDirectory());
-				Application.EnableVisualStyles();
-				Application.SetCompatibleTextRenderingDefault(false);
-				Application.Run(new Forms.frmMain());
-				//It'll update in the background while it's running. The update is in effect the next time they restart app.
-				MainAsync().Wait();
-			}
-			catch (Exception e)
-			{
-				Log.Error(e);
-			}
-		} 
+    public static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            try
+            {
+                var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                Settings.Default.filesDir = Path.Combine(appDataPath, "Project Altis");
+                if(!Directory.Exists(Settings.Default.filesDir))
+                    Directory.CreateDirectory(Settings.Default.filesDir);
+                Directory.SetCurrentDirectory(Settings.Default.filesDir);
+                Log.Initialize(LogType.Info);
+                Log.Info("Loaded altis launcher v" + typeof(Program).Assembly.GetName().Version);
+                Log.Info("Current time: " + DateTime.Now.ToLongDateString() + "|" + DateTime.Now.TimeOfDay);
+                Log.Info("Working directory: " + Directory.GetCurrentDirectory());
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmMain());
+                //It'll update in the background while it's running. The update is in effect the next time they restart app.
+                MainAsync().Wait();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
+        }
 
-		private static async Task MainAsync()
-		{
-			using (var manager = new UpdateManager("https://judge2020.com/altis"))
-			{
-				Log.Info("checking for update: " + manager.RootAppDirectory);
-				await manager.UpdateApp();
-			}
-		}
-	}
+        private static async Task MainAsync()
+        {
+            using (var manager = new UpdateManager("https://judge2020.com/altis"))
+            {
+                Log.Info("checking for update: " + manager.RootAppDirectory);
+                await manager.UpdateApp();
+            }
+        }
+    }
 }
