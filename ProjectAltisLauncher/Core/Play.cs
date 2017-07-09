90da2f169408f23ea708168c3586f7c623dd7809
ProbablyBen <iBapple4@gmail.com>
2017-07-09 00:09:06 -0400

    I can't believe I forgot to commit the file

--- ProjectAltisLauncher/Core/Play.cs
+++ ProjectAltisLauncher/Core/Play.cs
@@ -6,51 +6,47 @@ using System.ComponentModel;
 
 namespace ProjectAltis.Core
 {
-	public static class Play
-	{
-		/// <summary>
-		/// Launches the game.
-		/// </summary>
-		/// <param name="username">The username.</param>
-		/// <param name="password">The password.</param>
-		public static void LaunchGame(string username, string password, FrmMain frmInstance)
-		{
-			try
-			{
-				Log.Info("Starting Toontown: " + username);
-				Environment.SetEnvironmentVariable("TT_USERNAME", username);
-				Environment.SetEnvironmentVariable("TT_PASSWORD", password);
-				Environment.SetEnvironmentVariable("TT_GAMESERVER", "gs1.projectaltis.com");
-				Log.Info("Successfully set Environment variables.");
-				ProcessStartInfo startInfo = new ProcessStartInfo();
-				if (!Properties.Settings.Default.wantsGameDebug)
-				{
-					startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-				}
-				startInfo.FileName = "ProjectAltis";
-				Process altis = Process.Start(startInfo);
+    public static class Play
+    {
+        /// <summary>
+        /// Launches the game.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        public static void LaunchGame(string username, string password, FrmMain frmInstance)
+        {
+            try
+            {
+                Log.Info("Starting Toontown: " + username);
+                Environment.SetEnvironmentVariable("TT_USERNAME", username);
+                Environment.SetEnvironmentVariable("TT_PASSWORD", password);
+                Environment.SetEnvironmentVariable("TT_GAMESERVER", "gs1.projectaltis.com");
+                Log.Info("Successfully set Environment variables.");
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                if (!Properties.Settings.Default.wantsGameDebug)
+                {
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                }
+                startInfo.FileName = "ProjectAltis";
+                Process altis = Process.Start(startInfo);
 
-				frmInstance.BeginInvoke((MethodInvoker)delegate
-				{
-					Log.Info("Game process ended.");
-					frmInstance.Hide();
-					altis.WaitForExit();
-					frmInstance.lblNowDownloading.Text = "Thanks for playing!";
-					frmInstance.Show();
-				});
-			}
-			catch (Win32Exception ex)
-			{
-				Log.Error("Win32Exception thrown. Possibly older os?");
-				Log.Error(ex);
-				MessageBox.Show("An error has occured starting the game!\nIt is possible that you are running a legacy system.\nIt has been logged in the launcher's log.");
-			}
-			catch (Exception ex)
-			{
-				Log.Error("Error when starting game!");
-				Log.Error(ex);
-				MessageBox.Show("An unknown error occured when starting the game!\n It has been logged in the launcher's log.");
-			}
+                for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
+                {
+                    if (Application.OpenForms[i] is FrmMain)
+                    {
+                        Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
+                        {
+                            Application.OpenForms[i]?.Hide();
+                        });
+                    }
+                    else
+                    {
+                        Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
+                        {
+                            Application.OpenForms[i]?.Dispose();
+                        });
+                    }
+                }
 
                 frmInstance.BeginInvoke((MethodInvoker)delegate
                 {
