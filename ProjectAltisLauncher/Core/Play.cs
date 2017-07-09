4e3fb390b02c1d3dff3e1e2d9e15a5381fffef3c
ProbablyBen <iBapple4@gmail.com>
2017-07-09 00:36:19 -0400

    Bug fixed :)

--- ProjectAltisLauncher/Core/Play.cs
+++ ProjectAltisLauncher/Core/Play.cs
@@ -32,26 +32,28 @@ namespace ProjectAltis.Core
 
                 for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
                 {
+                    int i1 = i; // Modified Closure for delegates. Can't directly access the index variable.
+
                     if (Application.OpenForms[i] is FrmMain)
                     {
                         Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
                         {
-                            Application.OpenForms[i]?.Hide();
+                            Application.OpenForms[i1]?.Hide();
                         });
                     }
                     else
                     {
                         Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
                         {
-                            Application.OpenForms[i]?.Dispose();
+                            Application.OpenForms[i1]?.Dispose();
                         });
                     }
                 }
 
                 frmInstance.BeginInvoke((MethodInvoker)delegate
                 {
+                    altis?.WaitForExit();
                     Log.Info("Game process ended.");
-                    altis.WaitForExit();
                     frmInstance.lblNowDownloading.Text = "Thanks for playing!";
                     frmInstance.Show();
                 });
