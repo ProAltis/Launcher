9407c66a4778e9c335c0df3db32eaaa805c40d7b
ProbablyBen <iBapple4@gmail.com>
2017-07-09 16:23:53 -0400

    Cleanup

--- ProjectAltisLauncher/Core/Play.cs
+++ ProjectAltisLauncher/Core/Play.cs
@@ -13,42 +13,13 @@ namespace ProjectAltis.Core
         /// </summary>
         /// <param name="username">The username.</param>
         /// <param name="password">The password.</param>
+        /// <param name="frmInstance">FrmMain instance.</param>
         public static void LaunchGame(string username, string password, FrmMain frmInstance)
         {
             try
             {
-                Log.Info("Starting Toontown: " + username);
-                Environment.SetEnvironmentVariable("TT_USERNAME", username);
-                Environment.SetEnvironmentVariable("TT_PASSWORD", password);
-                Environment.SetEnvironmentVariable("TT_GAMESERVER", "gs1.projectaltis.com");
-                Log.Info("Successfully set Environment variables.");
-                ProcessStartInfo startInfo = new ProcessStartInfo();
-                if (!Properties.Settings.Default.wantsGameDebug)
-                {
-                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                }
-                startInfo.FileName = "ProjectAltis";
-                Process altis = Process.Start(startInfo);
-
-                for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
-                {
-                    int i1 = i; // Modified Closure for delegates. Can't directly access the index variable.
-
-                    if (Application.OpenForms[i] is FrmMain)
-                    {
-                        Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
-                        {
-                            Application.OpenForms[i1]?.Hide();
-                        });
-                    }
-                    else
-                    {
-                        Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
-                        {
-                            Application.OpenForms[i1]?.Dispose();
-                        });
-                    }
-                }
+                Process altis = StartAltis(username, password);
+                HideAllForms();
 
                 frmInstance.BeginInvoke((MethodInvoker)delegate
                 {
@@ -71,6 +42,45 @@ namespace ProjectAltis.Core
                 MessageBox.Show("An unknown error occured when starting the game!\n It has been logged in the launcher's log.");
             }
 
+        }
+
+        public static Process StartAltis(string username, string password)
+        {
+            Log.Info("Starting Toontown: " + username);
+            Environment.SetEnvironmentVariable("TT_USERNAME", username);
+            Environment.SetEnvironmentVariable("TT_PASSWORD", password);
+            Environment.SetEnvironmentVariable("TT_GAMESERVER", "gs1.projectaltis.com");
+            Log.Info("Successfully set Environment variables.");
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            if (!Properties.Settings.Default.wantsGameDebug)
+            {
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            }
+            startInfo.FileName = "ProjectAltis";
+            return Process.Start(startInfo);
+        }
+
+        public static void HideAllForms()
+        {
+            for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
+            {
+                int i1 = i; // Modified Closure for delegates. Can't directly access the index variable.
+
+                if (Application.OpenForms[i] is FrmMain)
+                {
+                    Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
+                    {
+                        Application.OpenForms[i1]?.Hide();
+                    });
+                }
+                else
+                {
+                    Application.OpenForms[i]?.BeginInvoke((MethodInvoker)delegate
+                    {
+                        Application.OpenForms[i1]?.Dispose();
+                    });
+                }
+            }
         }
     }
 }
