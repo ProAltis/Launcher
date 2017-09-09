b33da617df7f54da198ac9a0efc6c2fc90b8a9d0
judge2020 <judge2020@judge2020.com>
2017-09-08 23:16:59 -0400

    Content pack manifest definition

---
+++ ProjectAltisLauncher/Manifests/ContentPack.cs
@@ -0,0 +1,36 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+using System.Collections.Generic;
+
+namespace ProjectAltis.Manifests
+{
+    public class ContentPackResponse
+    {
+        public List<ContentPackDetail> data { get; set; }
+    }
+    
+    
+    public class ContentPackDetail
+    {
+        public string name { get; set; }
+
+        public string author { get; set; }
+        
+        public string description { get; set; }
+
+        public List<ContentPackVersion> versions { get; set; }
+    }
+
+    public class ContentPackVersion
+    {
+        public int version { get; set; }
+
+        public string displayversion { get; set; }
+
+        public string updates { get; set; }
+
+        public string size { get; set; }
+
+        public string download { get; set; }
+    }
+}
